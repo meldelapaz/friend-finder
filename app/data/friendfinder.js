a532eb3089f793fc63fd70d12b783b506ad23ccd
meldelapaz <meldelapaz.17@gmail.com>
2017-06-27 22:27:52 -0400

    started friendfinder.js

---
+++ app/data/friendfinder.js
@@ -0,0 +1,27 @@
+var animals = [
+    {
+        "name": "Dolphin"
+        "photo": "http://img.thesun.co.uk/multimedia/archive/01603/Bottlenose_dolphin_1603341a.jpg",
+        "scores":
+    },
+    {
+        "name": "Snake"
+        "photo": "https://fthmb.tqn.com/EcWQf9c5C2o8NUV0Y81T6cBV04w=/3000x2882/filters:no_upscale():fill(transparent,1)/about/Rattle-Snake-56a7a3bb5f9b58b7d0ec57fe.jpg",
+        "scores":
+    },
+    {
+        "name": "Tiger"
+        "photo": "http://images.mid-day.com/images/2017/mar/08tiger-bengal-l.jpg",
+        "scores":
+    },
+    {
+        "name": "Sloth"
+        "photo": "http://www.rd.com/wp-content/uploads/sites/2/2016/04/sloths-slide1SamTrull.jpg",
+        "scores":
+    },
+    {
+        "name": "Bear"
+        "photo": "http://static.boredpanda.com/blog/wp-content/uploads/2016/09/mother-bear-cubs-animal-parenting-21-57e3a2161d7f7__880.jpg",
+        "scores":
+    }
+]
--- server.js
+++ server.js
@@ -1,2 +1,24 @@
-var express = require('express');
-var bodyParser
+//BASIC NPM PACKAGES
+var express    = require('express');
+var bodyParser = require('body-parser');
+var path       = require('path');
+
+
+//SETS UP THE EXPRESS APP
+var PORT = 8080;
+var app  = express();
+
+//SETS UP THE EXPRESS APP TO HANDLE DATA PARSING
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: 'application/vnd.api.json'}));
+
+app.use(express.static('app/public'));
+
+require('./app/routing/html-routes.js')(app);
+require('./app/routing/api-routes.js')(app);
+
+app.listen(PORT, function(){
+    console.log('Server listening on %d', PORT)
+});
